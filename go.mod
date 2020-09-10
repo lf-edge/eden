95ce8ac69b030dde21de1568f1a36e44a4b96a9a
Avi Deitcher <avi@deitcher.net>
2020-09-10 21:28:36 +0300

    add local docker registry (#254)
    Signed-off-by: Avi Deitcher <avi@deitcher.net>

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.12
 
 require (
 	github.com/Microsoft/hcsshim v0.8.9 // indirect
+	4d63.com/gochecknoinits v0.0.0-20200108094044-eb73b47b9fc4 // indirect
 	github.com/amitbet/vncproxy v0.0.0-20200118084310-ea8f9b510913
 	github.com/containerd/continuity v0.0.0-20200710164510-efbc4488d8fe // indirect
 	github.com/docker/distribution v2.7.1+incompatible
@@ -15,6 +16,7 @@ require (
 	github.com/go-redis/redis/v7 v7.2.0
 	github.com/golang/protobuf v1.4.0-rc.4.0.20200313231945-b860323f09d0
 	github.com/google/go-containerregistry v0.0.0-20200331213917-3d03ed9b1ca2
+	github.com/gordonklaus/ineffassign v0.0.0-20200809085317-e36bfde3bb78 // indirect
 	github.com/lf-edge/adam v0.0.0-20200502191324-bedd2e5e0d61
 	github.com/lf-edge/eden/eserver v0.0.0-00010101000000-000000000000
 	github.com/lf-edge/eve/api/go v0.0.0-20200904022325-a578a9bf320b
@@ -25,11 +27,13 @@ require (
 	github.com/rn/iso9660wrap v0.0.0-20180101235755-3a04f8ca150a
 	github.com/rogpeppe/go-internal v1.6.0
 	github.com/satori/go.uuid v1.2.1-0.20181028125025-b2ce2384e17b
+	github.com/securego/gosec v0.0.0-20200401082031-e946c8c39989 // indirect
 	github.com/sirupsen/logrus v1.4.2
 	github.com/spf13/cobra v1.0.0
 	github.com/spf13/viper v1.7.1
+	github.com/walle/lll v1.0.1 // indirect
 	golang.org/x/crypto v0.0.0-20200220183623-bac4c82f6975
-	golang.org/x/net v0.0.0-20191004110552-13f9640d40b9
+	golang.org/x/net v0.0.0-20200226121028-0de0cce0169b
 	golang.org/x/oauth2 v0.0.0-20190604053449-0f29369cfe45
 	google.golang.org/api v0.13.0
 	gopkg.in/errgo.v2 v2.1.0
--- pkg/utils/eden.go
+++ pkg/utils/eden.go
@@ -5,11 +5,6 @@ import (
 	"crypto/tls"
 	"encoding/json"
 	"fmt"
-	"github.com/lf-edge/adam/pkg/x509"
-	"github.com/lf-edge/eden/eserver/api"
-	"github.com/lf-edge/eden/pkg/defaults"
-	"github.com/nerd2/gexto"
-	log "github.com/sirupsen/logrus"
 	"io/ioutil"
 	"net/http"
 	"os"
@@ -19,6 +14,12 @@ import (
 	"strconv"
 	"strings"
 	"time"
+
+	"github.com/lf-edge/adam/pkg/x509"
+	"github.com/lf-edge/eden/eserver/api"
+	"github.com/lf-edge/eden/pkg/defaults"
+	"github.com/nerd2/gexto"
+	log "github.com/sirupsen/logrus"
 )
 
 //StartRedis function run redis in docker with mounted redisPath:/data
@@ -191,6 +192,77 @@ func StatusAdam() (status string, err error) {
 	state, err := StateContainer(defaults.DefaultAdamContainerName)
 	if err != nil {
 		return "", fmt.Errorf("error in get state of adam container: %s", err)
+	}
+	if state == "" {
+		return "container doesn't exist", nil
+	}
+	return state, nil
+}
+
+//StartRegistry function run registry in docker
+func StartRegistry(port int, tag, registryPath string, opts ...string) (err error) {
+	containerName := defaults.DefaultRegistryContainerName
+	ref := defaults.DefaultRegistryContainerRef
+	serviceName := "registry"
+	portMap := map[string]string{"5000": strconv.Itoa(port)}
+	cmd := []string{}
+	for _, el := range opts {
+		cmd = append(cmd, el)
+	}
+	volumeMap := map[string]string{"/var/lib/registry": registryPath}
+	state, err := StateContainer(containerName)
+	if err != nil {
+		return fmt.Errorf("error in get state of %s container: %s", serviceName, err)
+	}
+	if state == "" {
+		if err := CreateAndRunContainer(containerName, ref+":"+tag, portMap, volumeMap, cmd); err != nil {
+			return fmt.Errorf("error in create %s container: %s", serviceName, err)
+		}
+	} else if !strings.Contains(state, "running") {
+		if err := StartContainer(containerName); err != nil {
+			return fmt.Errorf("error in restart %s container: %s", serviceName, err)
+		}
+	}
+	return nil
+}
+
+// StopRegistry function stop registry container
+func StopRegistry(rm bool) (err error) {
+	containerName := defaults.DefaultRegistryContainerName
+	serviceName := "registry"
+	state, err := StateContainer(containerName)
+	if err != nil {
+		return fmt.Errorf("error in get state of %s container: %s", serviceName, err)
+	}
+	if !strings.Contains(state, "running") {
+		if rm {
+			if err := StopContainer(containerName, true); err != nil {
+				return fmt.Errorf("error in rm %s container: %s", serviceName, err)
+			}
+		}
+	} else if state == "" {
+		return nil
+	} else {
+		if rm {
+			if err := StopContainer(containerName, false); err != nil {
+				return fmt.Errorf("error in rm %s container: %s", serviceName, err)
+			}
+		} else {
+			if err := StopContainer(containerName, true); err != nil {
+				return fmt.Errorf("error in rm %s container: %s", serviceName, err)
+			}
+		}
+	}
+	return nil
+}
+
+// StatusRegistry function return status of registry
+func StatusRegistry() (status string, err error) {
+	containerName := defaults.DefaultRegistryContainerName
+	serviceName := "registry"
+	state, err := StateContainer(containerName)
+	if err != nil {
+		return "", fmt.Errorf("error in get state of %s container: %s", serviceName, err)
 	}
 	if state == "" {
 		return "container doesn't exist", nil
