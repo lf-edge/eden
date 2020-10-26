4f480c46fe0a22cce5c4cabcf34f2a16af498f40
Oleg Sadov <oleg.sadov@gmail.com>
2020-10-26 17:00:30 +0300

    New network tests
    Signed-off-by: Oleg Sadov <oleg.sadov@gmail.com>

--- tests/eclient/eden+eclient.sh
+++ tests/eclient/eden+eclient.sh
@@ -7,22 +7,11 @@ EDEN=./eden
 if $EDEN config get --key eve.hostfwd | grep -v 2223
 then
   echo Adding 2223 port for eclient\'s SSH to EDEN config
-  PORTS=$($EDEN config get --key eve.hostfwd | sed 's/map\[\(.*\)\]/\1/; s/^/2223:2223 /')
+  PORTS=$($EDEN config get default --key eve.hostfwd | sed 's/map\[\(.*\)\]/{"\1 2223:2223"}/;s/ /","/g;s/:/":"/g')
   $EDEN config set default --key eve.hostfwd --value "$PORTS"
+  $EDEN config get default --key eve.hostfwd
+  echo $EDEN stop
+  $EDEN stop
+  echo $EDEN start
+  $EDEN start
 fi
-
-grep 'hostfwd = "tcp::2223-:2223"' ~/.eden/default-qemu.conf && exit
-
-echo Adding 2223 port for eclient\'s SSH to EDEN QEMU config
-ed ~/.eden/default-qemu.conf <<END
-/hostfwd = "tcp::2222-:22"
-a
-  hostfwd = "tcp::2223-:2223"
-.
-wq
-END
-
-echo $EDEN stop
-$EDEN stop
-echo $EDEN start
-$EDEN start
