a39c81226cddaed85938bd81e4b1469b9be0268a
Vitaliy Kuznetsov <ahil52_25@mail.rum>
2020-11-20 03:10:15 +0300

    fix etus
    Signed-off-by: Vitaliy Kuznetsov <ahil52_25@mail.rum>

--- tests/io_perfomance/image/run-test.sh
+++ tests/io_perfomance/image/run-test.sh
@@ -8,21 +8,21 @@ git config --global user.email "fio_test@example.com"
 git config --global user.name "FIO"
 
 #File management
-mkdir ~/$GITREPO/$foldername
-mkdir ~/$GITREPO/$foldername/configs
-mkdir ~/$GITREPO/$foldername/configs/test-results
-touch ~/$GITREPO/$foldername/SUMMARY.csv
-cp README.md ~/$GITREPO/$foldername/
-cp config.fio ~/$GITREPO/$foldername/configs/
+mkdir ~/"$GITREPO"/"$foldername"
+mkdir ~/"$GITREPO"/"$foldername"/configs
+mkdir ~/"$GITREPO"/"$foldername"/configs/test-results
+touch ~/"$GITREPO"/"$foldername"/SUMMARY.csv
+cp README.md ~/"$GITREPO"/"$foldername"/
+cp config.fio ~/"$GITREPO"/"$foldername"/configs/
 
 #Create a snapshot of the hardware
-lshw -short > ~/$GITREPO/$foldername/HARDWARE.cfg
+lshw -short > ~/"$GITREPO"/"$foldername"/HARDWARE.cfg
 
 #Running FIO
-fio config.fio > ~/$GITREPO/$foldername/configs/test-results/fio-result
+fio config.fio > ~/"$GITREPO"/"$foldername"/configs/test-results/fio-result
 
 #Create a new branch in the GIT repository and push the changes
-(cd ~/$GITREPO/ && git checkout -b $foldername && git add ~/$GITREPO/$foldername && git commit -m "fio-results" && git push --set-upstream origin $foldername)
+(cd ~/"$GITREPO"/ && git checkout -b "$foldername" && git add ~/"$GITREPO"/"$foldername" && git commit -m "fio-results" && git push --set-upstream origin "$foldername")
  
 
 
