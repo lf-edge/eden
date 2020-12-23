ade89156777f08ae59828dd683af39c5577a19a8
Vitaliy Kuznetsov <ahil52_25@mail.ru>
2020-12-23 15:32:02 +0300

    Fix run fio
    Signed-off-by: Vitaliy Kuznetsov <ahil52_25@mail.ru>

--- tests/io_perfomance/image/run-test.sh
+++ tests/io_perfomance/image/run-test.sh
@@ -2,7 +2,7 @@
 if [ -z "$GIT_BRANCH" ]; then FOLDER=FIO-tests-$(date +%H-%M-%d-%m-%Y)-"$EVE_VERSION"; else FOLDER="$GIT_BRANCH"; fi
 export FOLDERNAME="$FOLDER"
 
-#Git configurate
+# Git configurate
 echo "Started configuring GitHub"
 git config --global http.sslverify false
 (cd ~ && git clone https://"$GIT_LOGIN":"$GIT_TOKEN"@github.com/"$GIT_LOGIN"/"$GIT_REPO")
@@ -10,7 +10,7 @@ git config --global user.email "fio_test@example.com"
 git config --global user.name "FIO"
 echo "GitHub configuration is done"
 
-#File management
+# File management
 echo "Started setting up directories"
 mkdir ~/"$GIT_REPO"/"$FOLDERNAME"
 mkdir ~/"$GIT_REPO"/"$FOLDERNAME"/Configs
@@ -21,22 +21,22 @@ cp README.md ~/"$GIT_REPO"/"$FOLDERNAME"/
 cp config.fio ~/"$GIT_REPO"/"$FOLDERNAME"/Configs/
 echo "Setting up directories is end"
 
-#Create a snapshot of the hardware
+# Create a snapshot of the hardware
 lshw -short > ~/"$GIT_REPO"/"$FOLDERNAME"/HARDWARE.cfg
 
-#Running IOSTAT
+# Running IOSTAT
 echo "Running IOSTAT"
 ./run-iostat.sh &
 
-#Running FIO
+# Running FIO
 echo "Running FIO"
-fio config.fio --output=~/"$GIT_REPO"/"$FOLDERNAME"/Configs/Test-results/fio-results --output-format=normal,json
+fio config.fio --output-format=normal,json > ~/"$GIT_REPO"/"$FOLDERNAME"/Configs/Test-results/fio-results
 
 echo "Result FIO generate start"
 ./fioconv ~/"$GIT_REPO"/"$FOLDERNAME"/Configs/Test-results/fio-results ~/"$GIT_REPO"/"$FOLDERNAME"/SUMMARY.csv
 echo "Result FIO generate done"
 
-#Create a new branch in the GIT repository and push the changes
+# Create a new branch in the GIT repository and push the changes
 echo "Create a branch and start posting results to GIT"
 (cd ~/"$GIT_REPO"/ && git checkout -b "$FOLDERNAME" && git add ~/"$GIT_REPO"/"$FOLDERNAME" && git commit -m "fio-results" && git push --set-upstream origin "$FOLDERNAME")
 echo "FIO tests are end branch:""$FOLDERNAME"
