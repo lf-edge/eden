e6b01d7a0553929e7c12039c5a64328ee90cff7d
Petr Fedchenkov <giggsoff@gmail.com>
2021-04-06 19:19:29 +0300

    redis password generator
    Signed-off-by: Petr Fedchenkov <giggsoff@gmail.com>

--- pkg/utils/params.go
+++ pkg/utils/params.go
@@ -1,8 +1,10 @@
 package utils
 
 import (
+	"math/rand"
 	"regexp"
 	"strings"
+	"time"
 )
 
 //GetParams parse line with regexp into map
@@ -18,4 +20,17 @@ func GetParams(line, regEx string) (paramsMap map[string]string) {
 		}
 	}
 	return
+}
+
+//GeneratePassword returns string with defined length and random characters
+func GeneratePassword(length int) string {
+	rand.Seed(time.Now().UnixNano())
+	chars := []rune("ABCDEFGHIJKLMNOPQRSTUVWXYZ" +
+		"abcdefghijklmnopqrstuvwxyz" +
+		"0123456789")
+	var b strings.Builder
+	for i := 0; i < length; i++ {
+		b.WriteRune(chars[rand.Intn(len(chars))])
+	}
+	return b.String()
 }
