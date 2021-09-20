4558d83fdbb969915caf886fa30a08936f0e3700
Petr Fedchenkov <giggsoff@gmail.com>
2021-09-20 16:59:30 +0300

    add avahi-daemon and nginx to eclient image (#680)
    Signed-off-by: Petr Fedchenkov <giggsoff@gmail.com>

--- tests/eclient/image/entrypoint.sh
+++ tests/eclient/image/entrypoint.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
 
+# configurable with envs
+if [ -z "$AVAHI_HOST_NAME" ]; then AVAHI_HOST_NAME=ubuntu-http-server; fi
+if [ -z "$AVAHI_DOMAIN_NAME" ]; then AVAHI_DOMAIN_NAME=local; fi
+
+sed -i "s/^#host-name=.*/host-name=$AVAHI_HOST_NAME/" /etc/avahi/avahi-daemon.conf
+sed -i "s/^host-name=.*/host-name=$AVAHI_HOST_NAME/" /etc/avahi/avahi-daemon.conf
+sed -i "s/^#domain-name=.*/domain-name=$AVAHI_DOMAIN_NAME/" /etc/avahi/avahi-daemon.conf
+sed -i "s/^domain-name=.*/domain-name=$AVAHI_DOMAIN_NAME/" /etc/avahi/avahi-daemon.conf
+sed -i 's/^publish-workstation=no/publish-workstation=yes/' /etc/avahi/avahi-daemon.conf
+sed -i 's/^use-ipv6=yes/use-ipv6=no/' /etc/avahi/avahi-daemon.conf
+
+service dbus start
+service avahi-daemon start
+service nginx start
+
 mkdir -p /run/sshd
 /usr/sbin/sshd -D
