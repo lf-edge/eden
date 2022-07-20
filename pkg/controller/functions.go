6668d569356853d6524f8fd5eeea047d59ac822e
Petr Fedchenkov <giggsoff@gmail.com>
2022-07-20 15:30:45 +0300

    Adjust tests to reduce load
    We can refactor switch_net_vlans test to use less apps and to not run
    multiple dnsmasq in time. Also we can slightly increase config interval.
    
    Signed-off-by: Petr Fedchenkov <giggsoff@gmail.com>

--- pkg/controller/functions.go
+++ pkg/controller/functions.go
@@ -103,7 +103,7 @@ func (cloud *CloudCtx) OnBoardDev(node *device.Ctx) error {
 			node.SetRemote(cloud.vars.EveRemote)
 			node.SetRemoteAddr(cloud.vars.EveRemoteAddr)
 			if !alreadyRegistered { //new node
-				node.SetConfigItem("timer.config.interval", "5")
+				node.SetConfigItem("timer.config.interval", "10")
 				node.SetConfigItem("timer.location.app.interval", "10")
 				node.SetConfigItem("timer.location.cloud.interval", "300")
 				node.SetConfigItem("app.allow.vnc", "true")
