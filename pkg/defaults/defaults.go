a9418e0b2b887a94ebcc3948a776e25fa82f4d91
Petr Fedchenkov <giggsoff@gmail.com>
2022-07-26 10:20:42 +0300

    Extend volumes test with purging and EVE-OS update
    Signed-off-by: Petr Fedchenkov <giggsoff@gmail.com>

--- pkg/defaults/defaults.go
+++ pkg/defaults/defaults.go
@@ -52,7 +52,7 @@ const (
 	DefaultRegistryPort         = 5050
 
 	//tags, versions, repos
-	DefaultEVETag               = "8.6.0" // DefaultEVETag tag for EVE image
+	DefaultEVETag               = "8.7.0" // DefaultEVETag tag for EVE image
 	DefaultAdamTag              = "0.0.39"
 	DefaultRedisTag             = "7"
 	DefaultRegistryTag          = "2.7"
