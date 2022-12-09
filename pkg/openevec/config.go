8df3cf3a53ab374e2e73023985064671fd0326f2
Pavel Abramov <uncle.decart@gmail.com>
2022-12-09 11:49:34 +0100

    Remove DirectLoad from RuntimeConfig
    See #827
    
    Signed-off-by: Pavel Abramov <uncle.decart@gmail.com>

--- pkg/openevec/config.go
+++ pkg/openevec/config.go
@@ -147,7 +147,6 @@ type RegistryConfig struct {
 }
 
 type RuntimeConfig struct {
-	DirectLoad        bool     `cobraflag:"direct"`
 	Mount             []string `cobraflag:"mount"`
 	Disks             []string `cobraflag:"disks"`
 	Registry          string   `cobraflag:"registry"`
