70a203b00f31043296b45357392bce855a0363da
Milan Lenco <milan@zededa.com>
2024-07-10 18:11:29 +0200

    Add Eden-SDN example for Local NI with multiple ports
    This example demonstrates Local network instance with multiple ports
    and multipath static IP routes.
    EVE API changes were introduced and described here:
    
    https://github.com/lf-edge/eve-api/pull/53
    
    Signed-off-by: Milan Lenco <milan@zededa.com>

--- sdn/vm/scripts/get-eve-ip.sh
+++ sdn/vm/scripts/get-eve-ip.sh
@@ -8,7 +8,7 @@ find_ip() {
   if [ -n "$NETNS" ]; then
     PREFIX="ip netns exec $NETNS "
   fi
-  $PREFIX arp -a | while read ARP_ENTRY; do
+  $PREFIX arp -an | while read ARP_ENTRY; do
       local ENTRY_IP="$(echo "$ARP_ENTRY" | cut -d ' ' -f 2 | tr -d '()')"
       local ENTRY_MAC="$(echo "$ARP_ENTRY" | cut -d ' ' -f 4)"
       if [ "$MAC" = "$ENTRY_MAC" ]; then
