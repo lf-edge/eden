0787153b35c6e8759f3030fbd58e4ae63e224690
Shahriyar Jalayeri <shahriyar@zededa.com>
2024-10-10 13:02:14 +0300

    NeoEden: use new LogTime* functions in eve test
    Signed-off-by: Shahriyar Jalayeri <shahriyar@zededa.com>

--- neo-eden/tests/eve/vcom_test.go
+++ neo-eden/tests/eve/vcom_test.go
@@ -4,21 +4,20 @@ import (
 	"strings"
 
 	"github.com/bloomberg/go-testgroup"
-	log "github.com/sirupsen/logrus"
 )
 
 func (grp *SmokeTests) TestVcomLinkTpmRequestEK(t *testgroup.T) {
-	log.Println("TestVcomLinkTpmRequestEK started")
-	defer log.Println("TestVcomLinkTpmRequestEK finished")
+	eveNode.LogTimeInfof("TestVcomLinkTpmRequestEK started")
+	defer eveNode.LogTimeInfof("TestVcomLinkTpmRequestEK finished")
 
 	t.Log("Checking if vcomlink is running on EVE")
 	stat, err := eveNode.EveRunCommand("eve exec pillar ss -l --vsock")
 	if err != nil {
-		t.Fatalf("Failed to check if vcomlink is running: %v", err)
+		eveNode.LogTimeFatalf("Failed to check if vcomlink is running: %v", err)
 	}
 	// vcomlink listens on port 2000 and host cid is 2.
 	// this is hacky way to check it is running, but it works ¯\_(ツ)_/¯
 	if !strings.Contains(string(stat), "2:2000") {
-		t.Fatalf("vcomlink is not running, ss output :\n%s", stat)
+		eveNode.LogTimeFatalf("vcomlink is not running, ss output :\n%s", stat)
 	}
 }
