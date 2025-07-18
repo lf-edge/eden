7f06de902a2405e726cfaa1a8e301bb50b0d09df
Paul Gaiduk <paulg@zededa.com>
2025-07-18 09:13:18 +0200

    vector: add TestWorkingConfig
    This test applies a filter config to filter out periodic logs from
    chrony and verifies that the filter is applied correctly and the logs
    are no longer appear at the controller.
    
    Signed-off-by: Paul Gaiduk <paulg@zededa.com>

--- tests/vector/vector_test.go
+++ tests/vector/vector_test.go
@@ -9,6 +9,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/lf-edge/eden/pkg/controller/elog"
 	tk "github.com/lf-edge/eden/pkg/evetestkit"
 	"github.com/lf-edge/eden/pkg/utils"
 )
@@ -18,6 +19,7 @@ var logT *testing.T
 
 const (
 	projectName = "vector"
+	logTimeout  = 2 * time.Minute // Timeout for log checks
 )
 
 func logFatalf(format string, args ...interface{}) {
@@ -39,6 +41,15 @@ func logInfof(format string, args ...interface{}) {
 	}
 }
 
+type stepCounter struct {
+	count int
+}
+
+func (s *stepCounter) AnnounceNext(msg string) {
+	s.count++
+	logInfof("STEP %d: %s", s.count, msg)
+}
+
 func TestMain(m *testing.M) {
 	logInfof("%s Test started", projectName)
 	defer logInfof("%s Test finished", projectName)
@@ -56,11 +67,10 @@ func TestMain(m *testing.M) {
 func TestFaultyConfig(t *testing.T) {
 	// Initialize the the logger to use testing.T instance
 	logT = t
+	steppy := &stepCounter{}
 
 	logInfof("TestFaultyConfig started")
 	defer logInfof("TestFaultyConfig finished")
-
-	step := 1
 
 	logInfof("secure the initial config")
 	if err := eveNode.GetConfig("/tmp/initial_config"); err != nil {
@@ -74,9 +84,7 @@ func TestFaultyConfig(t *testing.T) {
 		}
 	}()
 
-	logInfof("STEP %d: check vector is running", step)
-	step++
-
+	steppy.AnnounceNext("check vector is running")
 	cmd := "eve status | grep vector"
 	out, err := eveNode.EveRunCommand(cmd)
 	if err != nil {
@@ -88,9 +96,7 @@ func TestFaultyConfig(t *testing.T) {
 		logFatalf("Vector is not running on EVE node")
 	}
 
-	logInfof("STEP %d: get hash of the valid config", step)
-	step++
-
+	steppy.AnnounceNext("get hash of the valid config")
 	cmd = "sha256sum /persist/vector/config/vector.yaml"
 	out, err = eveNode.EveRunCommand(cmd)
 	if err != nil {
@@ -98,9 +104,7 @@ func TestFaultyConfig(t *testing.T) {
 	}
 	hashValidConfig := strings.Split(strings.TrimSpace(string(out)), " ")[0]
 
-	logInfof("STEP %d: set faulty vector config", step)
-	step++
-
+	steppy.AnnounceNext("set faulty vector config")
 	// Read the faulty config from the file
 	faultyConfig, err := os.ReadFile("testdata/faulty.yaml")
 	if err != nil {
@@ -114,17 +118,13 @@ func TestFaultyConfig(t *testing.T) {
 		},
 	)
 
-	logInfof("STEP %d: wait for the faulty config to be applied", step)
-	step++
-
+	steppy.AnnounceNext("wait for the faulty config to be applied")
 	// Wait for the faulty config to be applied
 	if err := eveNode.WaitForConfigApplied(60 * time.Second); err != nil {
 		logFatalf("Failed to wait for the faulty config to be applied: %v", err)
 	}
 
-	logInfof("STEP %d: check that vector is still running", step)
-	step++
-
+	steppy.AnnounceNext("check that vector is still running")
 	cmd = "eve status | grep vector"
 	out, err = eveNode.EveRunCommand(cmd)
 	if err != nil {
@@ -136,9 +136,7 @@ func TestFaultyConfig(t *testing.T) {
 		logFatalf("Vector is not running on EVE node after applying the faulty config")
 	}
 
-	logInfof("STEP %d: check vector is still using the valid config", step)
-	step++
-
+	steppy.AnnounceNext("check that vector is using the faulty config")
 	cmd = "sha256sum /persist/vector/config/vector.yaml"
 	out, err = eveNode.EveRunCommand(cmd)
 	if err != nil {
@@ -158,11 +156,10 @@ func TestFaultyConfig(t *testing.T) {
 func TestEmptyConfig(t *testing.T) {
 	// Initialize the the logger to use testing.T instance
 	logT = t
+	steppy := &stepCounter{}
 
 	logInfof("TestEmptyConfig started")
 	defer logInfof("TestEmptyConfig finished")
-
-	step := 1
 
 	logInfof("secure the initial config")
 	if err := eveNode.GetConfig("/tmp/initial_config"); err != nil {
@@ -176,9 +173,7 @@ func TestEmptyConfig(t *testing.T) {
 		}
 	}()
 
-	logInfof("STEP %d: check vector is running", step)
-	step++
-
+	steppy.AnnounceNext("check vector is running")
 	cmd := "eve status | grep vector"
 	out, err := eveNode.EveRunCommand(cmd)
 	if err != nil {
@@ -190,9 +185,7 @@ func TestEmptyConfig(t *testing.T) {
 		logFatalf("Vector is not running on EVE node")
 	}
 
-	logInfof("STEP %d: get hash of the current config", step)
-	step++
-
+	steppy.AnnounceNext("get hash of the current config")
 	cmd = "sha256sum /persist/vector/config/vector.yaml"
 	out, err = eveNode.EveRunCommand(cmd)
 	if err != nil {
@@ -200,9 +193,7 @@ func TestEmptyConfig(t *testing.T) {
 	}
 	hashCurrentConfig := strings.Split(strings.TrimSpace(string(out)), " ")[0]
 
-	logInfof("STEP %d: set empty vector config", step)
-	step++
-
+	steppy.AnnounceNext("set empty vector config")
 	eveNode.UpdateNodeGlobalConfig(
 		nil,
 		map[string]string{
@@ -210,17 +201,13 @@ func TestEmptyConfig(t *testing.T) {
 		},
 	)
 
-	logInfof("STEP %d: wait for the new config to be applied", step)
-	step++
-
+	steppy.AnnounceNext("wait for the new config to be applied")
 	// Wait for the new config to be applied
 	if err := eveNode.WaitForConfigApplied(60 * time.Second); err != nil {
 		logFatalf("Failed to wait for the new config to be applied: %v", err)
 	}
 
-	logInfof("STEP %d: check that vector is still running", step)
-	step++
-
+	steppy.AnnounceNext("check that vector is still running")
 	cmd = "eve status | grep vector"
 	out, err = eveNode.EveRunCommand(cmd)
 	if err != nil {
@@ -232,9 +219,7 @@ func TestEmptyConfig(t *testing.T) {
 		logFatalf("Vector is not running on EVE node after applying the empty config")
 	}
 
-	logInfof("STEP %d: check vector is using the default config", step)
-	step++
-
+	steppy.AnnounceNext("check vector is using the default config")
 	cmd = "eve exec vector sha256sum /etc/vector/vector.yaml"
 	out, err = eveNode.EveRunCommand(cmd)
 	if err != nil {
@@ -256,5 +241,98 @@ func TestEmptyConfig(t *testing.T) {
 		logFatalf("Still using the current config, not the default one!")
 	default:
 		logFatalf("Unexpected config hash: expected %s or %s, got %s", hashDefaultConfig, hashCurrentConfig, hashConfig)
+	}
+}
+
+func TestWorkingConfig(t *testing.T) {
+	// Initialize the the logger to use testing.T instance
+	logT = t
+	steppy := &stepCounter{}
+
+	logInfof("TestWorkingConfig started")
+	defer logInfof("TestWorkingConfig finished")
+
+	logInfof("secure the initial config")
+	if err := eveNode.GetConfig("/tmp/initial_config"); err != nil {
+		logFatalf("Failed to get initial config: %v", err)
+	}
+
+	defer func() {
+		logInfof("revert to the initial config")
+		if err := eveNode.SetConfig("/tmp/initial_config"); err != nil {
+			logFatalf("Failed to get back to the initial config: %v", err)
+		}
+	}()
+
+	steppy.AnnounceNext("check vector is running")
+	cmd := "eve status | grep vector"
+	out, err := eveNode.EveRunCommand(cmd)
+	if err != nil {
+		logFatalf("Failed to run ssh '%s': %v", cmd, err)
+	}
+	if strings.Contains(string(out), "RUNNING") {
+		logInfof("Vector is running on EVE node")
+	} else {
+		logFatalf("Vector is not running on EVE node")
+	}
+
+	steppy.AnnounceNext("set debug log level to see more logs flowing")
+	eveNode.UpdateNodeGlobalConfig(
+		nil,
+		map[string]string{
+			"debug.default.loglevel":        "debug",
+			"debug.default.remote.loglevel": "debug",
+		},
+	)
+	if err := eveNode.WaitForConfigApplied(60 * time.Second); err != nil {
+		logFatalf("Failed to wait for the faulty config to be applied: %v", err)
+	}
+
+	steppy.AnnounceNext(fmt.Sprintf("check that chrony logs are flowing (timeout %v)", logTimeout))
+	query := map[string]string{
+		"function": ".*chrony.*",
+	}
+	if err := eveNode.FindLogOnAdam(query, elog.LogNew, logTimeout); err != nil {
+		logFatalf("Failed to find chrony logs: %v", err)
+	}
+
+	steppy.AnnounceNext("set filter vector config")
+	filterConfig, err := os.ReadFile("testdata/filter.yaml")
+	if err != nil {
+		logFatalf("Failed to read vector config file: %v", err)
+	}
+	eveNode.UpdateNodeGlobalConfig(
+		nil,
+		map[string]string{
+			"vector.config": base64.StdEncoding.EncodeToString(filterConfig),
+		},
+	)
+	if err := eveNode.WaitForConfigApplied(60 * time.Second); err != nil {
+		logFatalf("Failed to wait for the faulty config to be applied: %v", err)
+	}
+
+	steppy.AnnounceNext("give vector some time to apply the filter config")
+	time.Sleep(30 * time.Second)
+
+	steppy.AnnounceNext("wait for vector to apply the filter config")
+	hashFilterConfig := fmt.Sprintf("%x", sha256.Sum256(filterConfig))
+	cmd = "sha256sum /persist/vector/config/vector.yaml"
+	out, err = eveNode.EveRunCommand(cmd)
+	if err != nil {
+		logFatalf("Failed to run ssh '%s': %v", cmd, err)
+	}
+	hashConfig := strings.Split(strings.TrimSpace(string(out)), " ")[0]
+	switch hashConfig {
+	case hashFilterConfig:
+		logInfof("Config got applied correctly")
+	default:
+		logFatalf("Unexpected config hash: expected %s, got %s", hashFilterConfig, hashConfig)
+	}
+
+	steppy.AnnounceNext(fmt.Sprintf("check that chrony logs don't appear anymore (timeout %v)", logTimeout))
+	if err := eveNode.FindLogOnAdam(query, elog.LogNew, logTimeout); err != nil {
+		logInfof("No chrony logs found, as expected")
+	} else {
+		logFatalf("Chrony logs found, but they should not be present with the filter config")
 	}
 }
